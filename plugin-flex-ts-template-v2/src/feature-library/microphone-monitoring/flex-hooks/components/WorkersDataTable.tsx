41adb5bcfc5fcf0b7dc69443f107c17bc577a3fe
Bernardo Tuyay <bbtuyay@bitbucket.org>
2025-12-04 15:56:04 +0800

    fix lint errors

--- plugin-flex-ts-template-v2/src/feature-library/microphone-monitoring/flex-hooks/components/WorkersDataTable.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/microphone-monitoring/flex-hooks/components/WorkersDataTable.tsx
@@ -16,40 +16,35 @@ export const componentHook = function addMicStatusColumn(flex: typeof Flex, mana
 
   const business_units = getBusinessUnit();
 
-  //flex.WorkersDataTable.Content.remove(MIC_STATUS_COLUMN_KEY);
+  //  flex.WorkersDataTable.Content.remove(MIC_STATUS_COLUMN_KEY);
 
   const workerAttributes = (manager.workerClient?.attributes ?? {}) as { business_unit: string };
   if (workerAttributes.business_unit && !business_units.includes(workerAttributes.business_unit)) {
     return;
-  } else if (
-    !workerAttributes.business_unit ||
-    workerAttributes.business_unit === '' ||
-    workerAttributes.business_unit === undefined ||
-    (workerAttributes.business_unit && business_units.includes(workerAttributes.business_unit))
-  ) {
-    flex.WorkersDataTable.Content.add(
-      <Flex.ColumnDefinition
-        key={MIC_STATUS_COLUMN_KEY}
-        header="Mic Status"
-        content={(item: any) => <MicStatusColumn worker={item.worker} />}
-        sortingFn={(rowA, rowB) => {
-          const micOrder: Record<string, number> = { OFF: 0, ON: 1 };
-          const getRank = (worker: any) => {
-            const value = worker?.attributes?.mic;
-            return micOrder[String(value).toUpperCase()] ?? 2;
-          };
-
-          const rankA = getRank(rowA.worker);
-          const rankB = getRank(rowB.worker);
-
-          if (rankA === rankB) {
-            return 0;
-          }
-
-          return rankA > rankB ? 1 : -1;
-        }}
-      />,
-      { sortOrder: 99 },
-    );
   }
+
+  flex.WorkersDataTable.Content.add(
+    <Flex.ColumnDefinition
+      key={MIC_STATUS_COLUMN_KEY}
+      header="Mic Status"
+      content={(item: any) => <MicStatusColumn worker={item.worker} />}
+      sortingFn={(rowA, rowB) => {
+        const micOrder: Record<string, number> = { OFF: 0, ON: 1 };
+        const getRank = (worker: any) => {
+          const value = worker?.attributes?.mic;
+          return micOrder[String(value).toUpperCase()] ?? 2;
+        };
+
+        const rankA = getRank(rowA.worker);
+        const rankB = getRank(rowB.worker);
+
+        if (rankA === rankB) {
+          return 0;
+        }
+
+        return rankA > rankB ? 1 : -1;
+      }}
+    />,
+    { sortOrder: 99 },
+  );
 };
